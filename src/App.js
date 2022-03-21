9077b3488b4b426b12f18faadc14cd9b603d6457
Obinna Ishmael Iloanya <81533820+Obilomania@users.noreply.github.com>
2022-03-22 03:35:26 +0800

    Index CSS build

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import { Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Experience from "./components/experience/Experience";
+import Footer from "./components/footer/Footer";
+import Navigation from "./components/navigation/Navigation";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Services from "./components/services/Services";
+import Testimonials from "./components/testimonials/Testimonials";
 
-function App() {
+ const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Navigation />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
