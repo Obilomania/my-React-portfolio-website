e4e69f5727fd2cb79e57a0f2110a6e26664d0368
Obinna Ishmael Iloanya <81533820+Obilomania@users.noreply.github.com>
2022-03-23 13:46:11 -0500

    Portfolio-Done

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,8 +1,87 @@
 import React from 'react'
+import './portfolio.css'
+import IMG1 from '../../images/portfolio1.jpg'
+import IMG2 from '../../images/portfolio2.jpg'
+import IMG3 from '../../images/portfolio3.jpg'
+import IMG4 from '../../images/portfolio4.jpg'
+import IMG5 from '../../images/portfolio5.png'
+import IMG6 from '../../images/portfolio6.jpg'
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section id='portfolio'>
+      <h5>My Works</h5>
+      <h2>Porfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-CTA">
+            <a href="https://github.com" className='btn'>Github</a>
+           <a href="https://github.com" className='btn btn-primary'>Live Demo</a>
+          </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG2} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-CTA">
+            <a href="https://github.com" className='btn'>Github</a>
+           <a href="https://github.com" className='btn btn-primary'>Live Demo</a>
+          </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG3} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-CTA">
+            <a href="https://github.com" className='btn'>Github</a>
+           <a href="https://github.com" className='btn btn-primary'>Live Demo</a>
+          </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG4} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-CTA">
+            <a href="https://github.com" className='btn'>Github</a>
+           <a href="https://github.com" className='btn btn-primary'>Live Demo</a>
+          </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG5} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-CTA">
+            <a href="https://github.com" className='btn'>Github</a>
+           <a href="https://github.com" className='btn btn-primary'>Live Demo</a>
+          </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG6} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-CTA">
+            <a href="https://github.com" className='btn'>Github</a>
+           <a href="https://github.com" className='btn btn-primary'>Live Demo</a>
+          </div>
+        </article>
+        
+      </div>
+    </section>
   )
 }
 
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,14 @@ import './contact.css'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        
+      </div>
+    </section>
   )
 }
 
