ac6df616ddc53d528ef75a97bae98a1ec8a40728
Obinna Ishmael Iloanya <81533820+Obilomania@users.noreply.github.com>
2022-03-24 04:32:48 +0800

    layout-complete

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './contact.css'
+import { MdOutlineMail } from 'react-icons/md'
+import { AiOutlineInstagram } from 'react-icons/ai'
+import { SiWhatsapp } from 'react-icons/si'
 
 const Contact = () => {
   return (
@@ -8,10 +11,32 @@ const Contact = () => {
       <h2>Contact Me</h2>
 
       <div className="container contact__container">
-        
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineMail className='contact__Icon'/>
+            <h4>Email</h4>
+            <h5>obilomania@gmail.com</h5>
+            <a href="mailto:obilomania@gmail.com">Send A Message</a>
+          </article>
+
+          <article className="contact__option">
+            <AiOutlineInstagram className='contact__Icon'/>
+            <h4>Instagram</h4>
+            <h5>@obilomania</h5>
+            <a href="https://www.instagram.com/obilomania/">Follow Me</a>
+          </article>
+
+          <article className="contact__option">
+            <SiWhatsapp className='contact__Icon'/>
+            <h4>Whatsapp</h4>
+            <h5>+2348033954540</h5>
+            <a href="https://wa.me/message/BYWVQLJ45P6XO1">Send A Message</a>
+          </article>
+        </div>
+      
       </div>
     </section>
   )
-}
+} 
 
 export default Contact
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { FaFacebookF } from 'react-icons/fa'
+import { FaTwitter } from 'react-icons/fa'
+import { AiOutlineInstagram } from 'react-icons/ai'
+import './footer.css'
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      {/* <a href="" className='footer-logo'><h2>ObiloMania</h2></a> */}
+
+      <ul className="footerLinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Experience</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+
+      <div className="footer_socials">
+        <a href="https://facebook.com"><FaFacebookF/></a>
+        <a href="https://instagram.com"><AiOutlineInstagram/></a>
+        <a href="https://twitter.com"><FaTwitter/></a>
+      </div>
+
+      <div className="copyright">
+        <small> &copy; Obinna for Obilomania </small>
+      </div>
+    </footer>
   )
 }
 
